297046c9d595619965b6de64576867680b8e675e
Alexander Ostrovsky <thealeos@gmail.com>
2016-05-03 15:19:31 +0300

    + PoemTitleCell

---
+++ Poetry/PoemTitleCell.swift
@@ -0,0 +1,24 @@
+//
+//  PoemTitleCell.swift
+//  Poetry
+//
+//  Created by Alexander Ostrovsky on 03.05.16.
+//  Copyright © 2016 Alexander Ostrovsky. All rights reserved.
+//
+
+import UIKit
+
+class PoemTitleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
