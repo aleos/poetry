5c83f0e5d7d596000fef0118103c91ad8a0a1236
Alexander Ostrovsky <thealeos@gmail.com>
2016-05-03 14:55:24 +0300

    + AppDelegate: set blogger api key when application starts

--- Poetry/AppDelegate.swift
+++ Poetry/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import GoogleAPIs
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if let apiKey = NSUserDefaults.standardUserDefaults().stringForKey("BloggerApiKey") {
+            Blogger.sharedInstance.apiKey = apiKey
+            Blogger.sharedInstance.key = apiKey
+        }
+        
         return true
     }
 
